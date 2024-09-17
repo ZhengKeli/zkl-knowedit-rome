8d0524ac38dea4fa971f2933eea2951484be543e
keli <zhengkeli2009@126.com>
2024-09-18 01:39:17 +0800

    优化了 get_context_templates 的代码

--- rome/prefixes.py
+++ rome/prefixes.py
@@ -7,23 +7,12 @@ def get_context_templates(model, tok, length_params):
     global CONTEXT_TEMPLATES_CACHE
 
     if CONTEXT_TEMPLATES_CACHE is None:
-        CONTEXT_TEMPLATES_CACHE = ["{}"] + [
-            x + ". {}"
-            for x in sum(
-                (
-                    generate_fast(
-                        model,
-                        tok,
-                        ["<|endoftext|>"],
-                        n_gen_per_prompt=n_gen,
-                        max_out_len=length,
-                    )
-                    for length, n_gen in length_params
-                ),
-                [],
-            )
-        ]
+        generated_prefixes_groups = [
+            generate_fast(model, tok, ["<|endoftext|>"], n_gen_per_prompt=n_gen, max_out_len=length)
+            for length, n_gen in length_params]
+        generated_prefixes = [prefix for group in generated_prefixes_groups for prefix in group]
 
+        CONTEXT_TEMPLATES_CACHE = ["{}"] + [x + ". {}" for x in generated_prefixes]
         print(f"Cached context templates {CONTEXT_TEMPLATES_CACHE}")
 
     return CONTEXT_TEMPLATES_CACHE
