6645953579fc3dfd5ea9b43a6cc2fe38001c9db1
keli <zhengkeli2009@126.com>
2024-09-18 01:31:09 +0800

    简化了 compute_k() 的代码

--- rome/compute_k.py
+++ rome/compute_k.py
@@ -8,23 +8,17 @@ from .request import TextRomeRequest
 
 def compute_k(
     model: AutoModelForCausalLM,
-    tok: AutoTokenizer,
+    tokenizer: AutoTokenizer,
     request: TextRomeRequest,
     hparams: ROMEHyperParams,
     layer: int,
     context_templates: list[str],
 ) -> torch.Tensor:
-    word_repr_args = dict(
+    return repr_tools.get_reprs_at_word_tokens(
         model=model,
-        tok=tok,
-        layer=layer,
+        tok=tokenizer,
         module_template=hparams.rewrite_module_tmp,
-        track="in")
-
-    cur_repr = repr_tools.get_reprs_at_word_tokens(
+        layer=layer,
         context_templates=[templ.format(request.prompt_template) for templ in context_templates],
         words=[request.subject for _ in range(len(context_templates))],
-        **word_repr_args,
-    ).mean(0)
-
-    return cur_repr
+        track="in").mean(0)
