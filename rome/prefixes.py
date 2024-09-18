eb350fe93eb03e67cee9799cbe23cf57e3a77bde
keli <zhengkeli2009@126.com>
2024-09-18 11:43:11 +0800

    增加了 iter_random_texts()

--- rome/prefixes.py
+++ rome/prefixes.py
@@ -1,3 +1,7 @@
+from typing import Iterator
+
+from transformers import AutoModelForCausalLM, PreTrainedTokenizer
+
 from .utils.generate import generate_fast
 
 CONTEXT_TEMPLATES_CACHE = None
@@ -7,12 +11,18 @@ def get_context_templates(model, tok, length_params):
     global CONTEXT_TEMPLATES_CACHE
 
     if CONTEXT_TEMPLATES_CACHE is None:
-        generated_prefixes_groups = [
-            generate_fast(model, tok, ["<|endoftext|>"], n_gen_per_prompt=n_gen, max_out_len=length)
-            for length, n_gen in length_params]
-        generated_prefixes = [prefix for group in generated_prefixes_groups for prefix in group]
+        generated_prefixes = iter_random_texts(model, tok, length_params)
 
         CONTEXT_TEMPLATES_CACHE = ["{}"] + [x + ". {}" for x in generated_prefixes]
         print(f"Cached context templates {CONTEXT_TEMPLATES_CACHE}")
 
     return CONTEXT_TEMPLATES_CACHE
+
+
+def iter_random_texts(
+    model: AutoModelForCausalLM,
+    tok: PreTrainedTokenizer,
+    length_params: tuple[int, int],
+) -> Iterator[str]:
+    for length, n_gen in length_params:
+        yield from generate_fast(model, tok, ["<|endoftext|>"], n_gen_per_prompt=n_gen, max_out_len=length)
