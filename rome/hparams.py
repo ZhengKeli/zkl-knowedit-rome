77613bb306e1b25b279958524e3950a3f2305444
keli <zhengkeli2009@126.com>
2024-09-18 15:02:03 +0800

    调整了 ROMEHyperParams 中参数的顺序

--- rome/hparams.py
+++ rome/hparams.py
@@ -1,19 +1,18 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(kw_only=True)
 class ROMEHyperParams:
     # Method
-    layer: int
     v_num_grad_steps: int
     v_lr: float
     v_weight_decay: float
     clamp_norm_factor: float
     kl_factor: float
-    mom2_adjustment: bool
     context_template_length_params: list[list[int]]
 
     # Module templates
+    layer: int
     rewrite_module_tmp: str
     layer_module_tmp: str
     mlp_module_tmp: str
@@ -22,6 +21,7 @@ class ROMEHyperParams:
     lm_head_module: str
 
     # Statistics
+    mom2_adjustment: bool
     mom2_dataset: str
     mom2_n_samples: int
     mom2_dtype: str
