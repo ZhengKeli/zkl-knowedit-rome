e3a58cb761874493af63430e17cfaa035c188ca0
keli <zhengkeli2009@126.com>
2024-09-23 17:48:16 +0800

    用 transformers.pipeline("text-generation",...) 实现 iter_random_texts

--- rome/prefixes.py
+++ rome/prefixes.py
@@ -1,8 +1,6 @@
 from typing import Iterable, Iterator
 
-from transformers import PreTrainedModel, PreTrainedTokenizer
-
-from .utils.generate import generate_fast
+from transformers import PreTrainedModel, PreTrainedTokenizer, pipeline
 
 
 def iter_random_prefixes(
@@ -21,4 +19,12 @@ def iter_random_texts(
     length_params: Iterable[tuple[int, int]],
 ) -> Iterator[str]:
     for length, n_gen in length_params:
-        yield from generate_fast(model, tokenizer, ["<|endoftext|>"], n_gen_per_prompt=n_gen, max_out_len=length)
+        pipe = pipeline("text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            device=model.device,
+            num_return_sequences=n_gen,
+            return_full_text=False,
+            max_new_tokens=length)
+        for result in pipe(""):
+            yield result["generated_text"]
