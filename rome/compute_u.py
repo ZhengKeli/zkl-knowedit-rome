966cb556e5f331585689f1af7f4d31e824029f1a
keli <zhengkeli2009@126.com>
2024-09-24 00:51:08 +0800

    将 prefixes_tokenized 和 rewriting_tokenized 移到 execute_rome()

--- rome/compute_u.py
+++ rome/compute_u.py
@@ -5,34 +5,30 @@ from transformers import PreTrainedModel, PreTrainedTokenizer
 from .compute_c import compute_c_inv
 from .compute_k import compute_k
 from .hparams import ROMEHyperParams
-from .rewriting import TextRomeRewriting, TokenizedRomeRewriting
+from .rewriting import TokenizedRomeRewriting
 
 
 def compute_u(
     hparams: ROMEHyperParams,
     model: PreTrainedModel,
     tokenizer: PreTrainedTokenizer,
-    prefixes: list[str],
-    rewriting: TextRomeRewriting,
+    prefixes: list[np.ndarray],
+    rewriting: TokenizedRomeRewriting,
     stats_dir: str,
 ) -> torch.Tensor:
-    layer = hparams.layer
-
-    rewriting_tokenized = TokenizedRomeRewriting.from_text_rewriting(rewriting, tokenizer)
-    prefixes_tokenized = [np.asarray(tokenizer.encode(prefix), dtype=np.int64) for prefix in prefixes]
     u = compute_k(
         hparams,
         model,
-        layer,
-        prefixes_tokenized,
-        rewriting_tokenized)
+        hparams.layer,
+        prefixes,
+        rewriting)
 
     # Apply inverse second moment adjustment
     if hparams.mom2_adjustment:
         c_inv = compute_c_inv(
             model,
             tokenizer,
-            hparams.rewrite_module_tmp.format(layer),
+            hparams.rewrite_module_tmp.format(hparams.layer),
             hparams.mom2_dataset,
             hparams.mom2_n_samples,
             hparams.mom2_dtype,
--- rome/compute_uv.py
+++ rome/compute_uv.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from transformers import PreTrainedModel, PreTrainedTokenizer
 
@@ -5,7 +6,7 @@ from .compute_u import compute_u
 from .compute_v import compute_v
 from .hparams import ROMEHyperParams
 from .prefixes import iter_random_prefixes
-from .rewriting import TextRomeRewriting
+from .rewriting import TextRomeRewriting, TokenizedRomeRewriting
 
 
 def execute_rome(
@@ -15,15 +16,16 @@ def execute_rome(
     hparams: ROMEHyperParams,
     stats_dir: str,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    # prefixes
     prefixes = list(iter_random_prefixes(model, tokenizer, hparams.context_template_length_params))
+    prefixes_tokenized = [np.asarray(tokenizer.encode(prefix), dtype=np.int64) for prefix in prefixes]
+    rewriting_tokenized = TokenizedRomeRewriting.from_text_rewriting(rewriting, tokenizer)
 
     u = compute_u(
         hparams,
         model,
         tokenizer,
-        prefixes,
-        rewriting,
+        prefixes_tokenized,
+        rewriting_tokenized,
         stats_dir)
 
     v = compute_v(
