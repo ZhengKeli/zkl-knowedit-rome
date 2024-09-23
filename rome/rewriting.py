fb685fd582f28a806b2dfda4cc4ac2333d2d289f
keli <zhengkeli2009@126.com>
2024-09-23 18:24:44 +0800

    增加了 TokenizedRomeRewriting

--- rome/rewriting.py
+++ rome/rewriting.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+import numpy as np
+from transformers import PreTrainedTokenizer
 from typing_extensions import overload
 
 
@@ -53,3 +55,32 @@ class TextRomeRewriting:
     @property
     def prompt_template(self) -> str:
         return self.prompt[:self.subject_head] + "{}" + self.prompt[self.subject_tail:]
+
+
+@dataclass
+class TokenizedRomeRewriting:
+    prompt: np.ndarray
+    target: np.ndarray
+    subject_head: int
+    subject_tail: int
+
+    @property
+    def subject(self) -> np.ndarray:
+        return self.prompt[self.subject_head:self.subject_tail]
+
+    @classmethod
+    def from_text_rewriting(cls, rewriting: TextRomeRewriting, tokenizer: PreTrainedTokenizer):
+        prefix_tokenized = np.asarray(tokenizer.encode(rewriting.prompt[:rewriting.subject_head]), dtype=np.int64)
+        suffix_tokenized = np.asarray(tokenizer.encode(rewriting.prompt[rewriting.subject_tail:]), dtype=np.int64)
+        subject_tokenized = np.asarray(tokenizer.encode(rewriting.subject), dtype=np.int64)
+        target_tokenized = np.asarray(tokenizer.encode(rewriting.target), dtype=np.int64)
+        prompt_tokenized = np.concatenate([prefix_tokenized, subject_tokenized, suffix_tokenized])
+
+        subject_head_tokenized = len(prefix_tokenized)
+        subject_tail_tokenized = subject_head_tokenized + len(subject_tokenized)
+
+        return cls(
+            prompt=prompt_tokenized,
+            target=target_tokenized,
+            subject_head=subject_head_tokenized,
+            subject_tail=subject_tail_tokenized)
