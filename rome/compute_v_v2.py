140f3a3408e7808c6bf2e1dd2027dcff5a0bc947
keli <zhengkeli2009@126.com>
2024-09-24 16:12:20 +0800

    在 compute_v_v2 中增加了计算 standard k,v 的一步，简化了代码

--- rome/compute_v_v2.py
+++ rome/compute_v_v2.py
@@ -4,13 +4,12 @@ import numpy as np
 import torch
 from transformers import PreTrainedModel, PreTrainedTokenizer
 
-from . import repr_tools
 from .hparams import ROMEHyperParams
 from .preserving import TextRomePreserving, TokenizedRomePreserving
 from .rewriting import TextRomeRewriting, TokenizedRomeRewriting
 from .utils import nethook
 from .utils.batching import stack_with_padding
-from .utils.hooks import forward_output_hook
+from .utils.hooks import StopForward, forward_output_hook
 from .utils.nethook import get_module
 
 
@@ -70,32 +69,33 @@ def compute_v(
         *rewritings_subject_token_index,
         *preservings_subject_token_index]
 
-    # Set up an optimization over a latent vector that, when output at the
-    # rewrite layer, i.e. hypothesized fact lookup location, will induce the
-    # target token to be predicted at the final layer.
-    target_init, preservings_log_probs_init = None, None
+    # compute k, v without prefixes
+    k: torch.Tensor | None = None
+    v: torch.Tensor | None = None
 
-    # Optimizer
-    delta: torch.Tensor | None = None
-    opt: torch.optim.Optimizer | None = None
+    def hook_func(_, inputs: tuple[torch.Tensor], output: torch.Tensor):
+        input, = inputs
+        nonlocal k, v
+        k = input[0, rewriting_tokenized.subject_tail - 1].clone()
+        v = output[0, rewriting_tokenized.subject_tail - 1].clone()
+        raise StopForward()
+
+    with torch.no_grad(), forward_output_hook(get_module(model, hparams.rewrite_module_tmp.format(layer)), hook_func):
+        model(torch.asarray(rewriting_tokenized.prompt, dtype=torch.int64, device=model.device))
+
+    assert isinstance(k, torch.Tensor)
+    assert isinstance(v, torch.Tensor)
+
+    preservings_log_probs_init: torch.Tensor | None = None
+    v_norm = torch.norm(v)
+
+    delta = torch.zeros(v.shape, dtype=v.dtype, device=v.device, requires_grad=True)
+    opt = torch.optim.Adam([delta], lr=hparams.v_lr)
 
     # Inserts new "delta" variable at the appropriate part of the computation
     def edit_output_fn(_, __, cur_out: torch.Tensor) -> torch.Tensor:
-        nonlocal target_init, delta, opt
-
-        # Store initial value of the vector of interest
-        if target_init is None:
-            print("Recording initial value of v*")
-            # Initial value is recorded for the clean sentence
-            target_init = cur_out[0, all_in_subject_token_index[0]].detach().clone()
-
-        if delta is None:
-            delta = torch.zeros(cur_out.shape[-1:], dtype=cur_out.dtype, device=cur_out.device, requires_grad=True)
-            opt = torch.optim.Adam([delta], lr=hparams.v_lr)
-
         for i, idx in enumerate(all_in_subject_token_index):
             cur_out[i, idx, :] += delta
-
         return cur_out
 
     # Execute optimization
@@ -125,7 +125,7 @@ def compute_v(
         rewriting_loss = -torch.mean(rewritings_out_tokens_log_prob)
 
         # Compute loss on regularization
-        regularization_loss = hparams.v_weight_decay * (torch.norm(delta) / torch.norm(target_init) ** 2)
+        regularization_loss = hparams.v_weight_decay * (torch.norm(delta) / v_norm ** 2)
         # regularization_loss = hparams.v_weight_decay * torch.norm(delta) ** 2
 
         loss = rewriting_loss + hparams.kl_factor * preserving_loss + regularization_loss
@@ -149,59 +149,18 @@ def compute_v(
         opt.zero_grad()
 
         # Project within L2 ball
-        max_norm = hparams.clamp_norm_factor * target_init.norm()
+        max_norm = hparams.clamp_norm_factor * v_norm
         if delta.norm() > max_norm:
             with torch.no_grad():
                 delta[...] = delta * max_norm / delta.norm()
 
-    target = target_init + delta
-
-    # Retrieve cur_input, the current input to the 2nd MLP layer, and
-    # cur_output, the original output of the 2nd MLP layer.
-    cur_input, cur_output = get_module_input_output_at_word(
-        model,
-        tokenizer,
-        layer,
-        input=rewriting.prompt_template,
-        word=rewriting.subject,
-        module_template=hparams.rewrite_module_tmp,
-    )
+    target = v + delta
 
     # Solving the linear system to compute the right vector
-    right_vector = (target - cur_output) / torch.dot(cur_input, left_vector)
-    print(f"Delta norm: {(target - cur_output).norm().item()}")
-    print(f"Change in target norm: {target_init.norm().item()} to {target.norm().item()} => {(target.norm() - target_init.norm()).item()}")
-    print(f"Division Factor: {torch.dot(cur_input, left_vector).item()}")
+    right_vector = (target - v) / torch.dot(k, left_vector)
+    print(f"Delta norm: {(target - v).norm().item()}")
+    print(f"Change in target norm: {v_norm.item()} to {target.norm().item()} => {(target.norm() - v_norm).item()}")
+    print(f"Division Factor: {torch.dot(k, left_vector).item()}")
     print(f"Right vector norm: {right_vector.norm()}")
 
     return right_vector
-
-
-def get_module_input_output_at_word(
-    model: PreTrainedModel,
-    tok: PreTrainedTokenizer,
-    layer: int,
-    input: str,
-    word: str,
-    module_template: str,
-) -> tuple[torch.Tensor]:
-    """
-    Retrieves detached representations for a word at the input and
-    output of a particular layer module.
-    """
-
-    word_repr_args = dict(
-        model=model,
-        tok=tok,
-        layer=layer,
-        module_template=module_template,
-    )
-    l_input, l_output = repr_tools.get_reprs_at_word_tokens(
-        track="both",
-        inputs=[input],
-        words=[word],
-        **word_repr_args,
-    )
-
-    l_input, l_output = l_input[0], l_output[0]
-    return l_input.detach(), l_output.detach()
