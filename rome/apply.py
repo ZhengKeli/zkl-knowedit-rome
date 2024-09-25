d9ad43db066fc293eda3755e0087a9902342ae22
keli <zhengkeli2009@126.com>
2024-09-25 15:05:12 +0800

    增加了 apply_left_right_to_module

--- rome/apply.py
+++ rome/apply.py
@@ -1,8 +1,9 @@
+import warnings
 from typing import Iterable
 
 import numpy as np
 import torch
-from transformers import PreTrainedModel
+from transformers import Conv1D, PreTrainedModel
 
 from .compute_left_right import compute_left_right
 from .hparams import ROMEHyperParams
@@ -23,14 +24,23 @@ def apply_rome_to_model(
     module = nethook.get_module(model, module_name)
 
     (left, right) = compute_left_right(model, module, rewriting, prefixes, preservings, hparams.v_delta, c_inv)
-    weight_delta = torch.outer(left, right)
-
-    apply_weight_delta(module, weight_delta)
+    apply_left_right_to_module(module, left, right)
 
 
-def apply_weight_delta(
+def apply_left_right_to_module(
     module: torch.nn.Module,
-    weight_delta: torch.Tensor,
+    left: torch.Tensor,
+    right: torch.Tensor,
 ):
-    with torch.no_grad():
-        module.weight[...] += weight_delta
+    if isinstance(module, Conv1D):
+        module.weight.data.add_(torch.outer(left, right))
+    elif isinstance(module, (torch.nn.Linear, torch.nn.LazyLinear)):
+        module.weight.data.add_(torch.outer(right, left))
+    else:
+        def hook(_: torch.nn.Module, inputs: tuple[torch.Tensor, ...], output: torch.Tensor):
+            k = inputs[0]
+            v_delta = torch.sum(k * left, dim=-1, keepdim=True) * right
+            return output + v_delta
+
+        module.register_forward_hook(hook)
+        warnings.warn(f"Not recognized module type {module}, fallback to use hook.")
