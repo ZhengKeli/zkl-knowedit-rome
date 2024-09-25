ca81c7e1fc10efa611eb64a9fb75d46aec882fbc
keli <zhengkeli2009@126.com>
2024-09-25 15:14:56 +0800

    调整了 RomeHparams 中各属性的顺序

--- rome/hparams.py
+++ rome/hparams.py
@@ -5,17 +5,16 @@ from .compute_v_delta import RomeComputeVDeltaHparams
 
 @dataclass(kw_only=True)
 class RomeHparams:
-    # Method
-    context_template_length_params: list[list[int]]
-
-    # Module templates
     rewrite_module_name: str
 
-    # v delta
-    v_delta: RomeComputeVDeltaHparams
+    # prefixes
+    context_template_length_params: list[list[int]]
 
-    # Statistics
+    # k
     mom2_adjustment: bool
     mom2_dataset: str
     mom2_n_samples: int
     mom2_dtype: str
+
+    # v delta
+    v_delta: RomeComputeVDeltaHparams
