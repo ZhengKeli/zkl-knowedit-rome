03befd1f57a881dd9cb0d7ec551a31efb4836fe5
keli <zhengkeli2009@126.com>
2024-10-09 16:30:24 +0800

    改进了 stack_with_padding 使其支持返回 mask

--- rome/utils/batching.py
+++ rome/utils/batching.py
@@ -6,8 +6,9 @@ from numpy.typing import ArrayLike
 
 def stack_with_padding(
     arrays: Iterable[np.ndarray],
-    pad: ArrayLike,
-) -> np.ndarray:
+    pad: ArrayLike, *,
+    return_mask: bool = False
+) -> np.ndarray | tuple[np.ndarray, np.ndarray]:
     arrays = tuple(arrays)
     max_len = max(len(array) for array in arrays)
 
@@ -17,4 +18,13 @@ def stack_with_padding(
         array_padded = np.concatenate([array, array_pad])
         arrays_padded.append(array_padded)
 
-    return np.stack(arrays_padded)
+    if not return_mask:
+        return np.stack(arrays_padded)
+
+    arrays_mask = []
+    for array in arrays:
+        array_ones = np.ones_like(array, dtype=bool)
+        array_zeros = np.zeros([max_len - len(array), *array.shape[1:]], dtype=bool)
+        array_mask = np.concatenate([array_ones, array_zeros])
+        arrays_mask.append(array_mask)
+    return np.stack(arrays_padded), np.stack(arrays_mask)
