55e691c576bc189c6489b97d3132f5a7f6e92569
keli <zhengkeli2009@126.com>
2024-10-10 11:41:32 +0800

    删除了无用的文件

--- rome/__init__.py
+++ rome/__init__.py
@@ -2,7 +2,6 @@ from .apply import apply_left_right_to_module
 from .compute_c import compute_c, RomeComputeCHParams
 from .compute_left_right import compute_left_right
 from .compute_v_delta import RomeComputeVDeltaHparams
-from .hparams import RomeHparams
 from .preserving import TextRomePreserving, TokenizedRomePreserving
 from .rewriting import TextRomeRewriting, TokenizedRomeRewriting
 from .prefixes import make_default_prefixes
