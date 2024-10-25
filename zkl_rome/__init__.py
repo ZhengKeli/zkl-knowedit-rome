6d1b99e103385d4f6d09e86b288135c1fb84b5e8
keli <zhengkeli2009@126.com>
2024-10-25 11:41:09 +0800

    added `compute_c_inv()`

--- zkl_rome/__init__.py
+++ zkl_rome/__init__.py
@@ -1,5 +1,6 @@
 from .apply import apply_left_right_to_module
 from .compute_c import ComputeCHparams, compute_c
+from .compute_c_inv import compute_c_inv
 from .compute_k_v import compute_k_v
 from .compute_left_right import compute_left_right
 from .compute_v_delta import ComputeVDeltaHparams, ComputeVDeltaMetrics, compute_v_delta
---
+++ zkl_rome/compute_c_inv.py
@@ -0,0 +1,24 @@
+from typing import Callable, Iterable
+
+import numpy as np
+import torch
+from transformers import PreTrainedModel
+
+from .compute_c import ComputeCHparams, ComputeCMetrics, compute_c
+
+
+def compute_c_inv(*,
+    model: PreTrainedModel,
+    module: torch.nn.Module,
+    compute_c_dataset: Iterable[np.ndarray],
+    compute_c_hparams: ComputeCHparams,
+    compute_c_callback: Callable[[ComputeCMetrics], None] | None = None,
+) -> torch.Tensor:
+    c = compute_c(
+        model=model,
+        module=module,
+        dataset=compute_c_dataset,
+        hparams=compute_c_hparams,
+        callback=compute_c_callback)
+    c_inv = torch.inverse(c)
+    return c_inv
